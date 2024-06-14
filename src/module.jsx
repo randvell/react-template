e813700d2de75e2b94b055001ca51cd1ab15958d
Nikita Zimin <n.zimin@aristos.pw>
2024-06-14 20:37:40 +0300

    React template

---
+++ src/module.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import testImg from './img/test.png';
+import './index.scss';
+
+const Module = () => (
+  <>
+    <h1>Hello, React!</h1>
+    <img src={testImg} />
+  </>
+);
+
+export default Module;
